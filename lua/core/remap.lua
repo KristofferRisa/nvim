6040215f0788add6b8e0d147f71b701f29c3a2a9
Kristoffer Risa <kristofferrisa@gmail.com>
2024-12-08 09:17:29 +0100

    neo-tree toggle remap

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>e', function()
+  require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() })
+end, { noremap = true, silent = true, desc = "Toggle NeoTree" })
