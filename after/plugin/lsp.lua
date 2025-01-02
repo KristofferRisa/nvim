574c9c4ba3c12cd28af5f97f6013283305acf517
Kristoffer Risa <kristofferrisa@gmail.com>
2025-01-02 07:25:00 +0100

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,6 +14,8 @@ lspconfig_defaults.capabilities = vim.tbl_deep_extend(
   require('cmp_nvim_lsp').default_capabilities()
 )
 
+require'lspconfig'.pyright.setup{}
+
 -- This is where you enable features that only work
 -- if there is a language server active in the file
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -42,9 +44,11 @@ require('lspconfig').ocamllsp.setup({})
 
 local cmp = require('cmp')
 
+cmp.register_source("easy-dotnet", require("easy-dotnet").package_completion_source)
 cmp.setup({
   sources = {
     {name = 'nvim_lsp'},
+    { name = 'easy-dotnet' },
   },
   snippet = {
     expand = function(args)
@@ -52,5 +56,95 @@ cmp.setup({
       vim.snippet.expand(args.body)
     end,
   },
-  mapping = cmp.mapping.preset.insert({}),
+  mapping = {
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
 })
+
+-- Easy-dotnet lsp configuration 
+--
+local function get_secret_path(secret_guid)
+  local path = ""
+  local home_dir = vim.fn.expand('~')
+  if require("easy-dotnet.extensions").isWindows() then
+    path = home_dir .. '\\AppData\\Roaming\\Microsoft\\UserSecrets\\' .. secret_guid .. "\\secrets.json"
+  else
+    path = home_dir .. "/.microsoft/usersecrets/" .. secret_guid .. "/secrets.json"
+  end
+  return path
+end
+
+local dotnet = require("easy-dotnet")
+
+dotnet.setup({
+  get_sdk_path = get_sdk_path,
+  test_runner = {
+    viewmode = "float",
+    enable_buffer_test_execution = true,
+    noBuild = true,
+    noRestore = true,
+    icons = {
+      passed = "",
+      skipped = "",
+      failed = "",
+      success = "",
+      reload = "",
+      test = "",
+      sln = "󰘐",
+      project = "󰘐",
+      dir = "",
+      package = "",
+    },
+    mappings = {
+      run_test_from_buffer = { lhs = "<leader>r", desc = "run test from buffer" },
+      filter_failed_tests = { lhs = "<leader>fe", desc = "filter failed tests" },
+      debug_test = { lhs = "<leader>d", desc = "debug test" },
+      go_to_file = { lhs = "g", desc = "go to file" },
+      run_all = { lhs = "<leader>R", desc = "run all tests" },
+      run = { lhs = "<leader>r", desc = "run test" },
+      peek_stacktrace = { lhs = "<leader>p", desc = "peek stacktrace of failed test" },
+      expand = { lhs = "o", desc = "expand" },
+      expand_all = { lhs = "E", desc = "expand all" },
+      collapse_all = { lhs = "W", desc = "collapse all" },
+      close = { lhs = "q", desc = "close test runner" },
+      refresh_testrunner = { lhs = "<C-r>", desc = "refresh test runner" }
+    },
+    additional_args = {}
+  },
+  terminal = function(path, action)
+    local commands = {
+      run = function()
+        return "dotnet run --project " .. path
+      end,
+      test = function()
+        return "dotnet test " .. path
+      end,
+      restore = function()
+        return "dotnet restore " .. path
+      end,
+      build = function()
+        return "dotnet build " .. path
+      end
+    }
+    local command = commands[action]() .. "\r"
+    vim.cmd("vsplit")
+    vim.cmd("term " .. command)
+  end,
+  secrets = {
+    path = get_secret_path
+  },
+  csproj_mappings = true,
+  fsproj_mappings = true,
+  auto_bootstrap_namespace = true
+})
+
+-- Additional commands and key mappings
+vim.api.nvim_create_user_command('Secrets', function()
+  dotnet.secrets()
+end, {})
+
+vim.keymap.set("n", "<C-p>", function()
+  dotnet.run_project()
+end)
